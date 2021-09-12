40309034be87099e5c3306002d5db392683e59c0
nobody <moisesalex132@gmail.com>
2021-09-11 22:12:16 -0600

    ➕ADD: nuevos paquetes

--- tu-lugarcito-back/src/app.js
+++ tu-lugarcito-back/src/app.js
@@ -1,48 +1,8 @@
 const app = require("./server");
-
 const env = require("dotenv");
-// const { sequelize } = require("./models/index");
-const { Role, User } = require("../src/database/db");
 
 env.config();
 
 app.listen(4000, () => {
   console.log("Server andando tuany");
-});
-
-app.post("/newUser", async (req, res) => {
-  let body = req.body;
-
-  try {
-    User.create({
-      username: body.username,
-      email: body.email,
-      password: body.password,
-    })
-      .then((user) => {
-        Role.create({
-          nameRole: body.nameRole,
-          userid: user.id,
-        }).then((result) => {
-          res.json({
-            ok: true,
-            message: "Usuario creado",
-            user,
-            result
-          });
-        });
-      })
-      .catch((Err) => {
-        res.json({
-          ok: false,
-          message: "ocurrio un error",
-          Err
-        });
-      });
-  } catch (error) {
-    res.status(500).json({
-      ok: false,
-      message: "ocurrio un  grave error",
-    });
-  }
 });
--- tu-lugarcito-back/src/server.js
+++ tu-lugarcito-back/src/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(require("./router/index"));
+app.use(cookieParser());
+app.use(morgan("dev"));
 
 module.exports = app;
