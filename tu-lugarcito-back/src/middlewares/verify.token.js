852f5f12e564b86d7e2ad94b5264dbe5ea29f61b
nobody <moisesalex132@gmail.com>
2021-09-17 16:57:29 -0600

    ➕ADD: middleware para validacion del token

---
+++ tu-lugarcito-back/src/middlewares/verify.token.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+let verify_token = (req, res, next) => {
+  let token = req.get("token");
+
+  if (!token) {
+    return res.status(401).json({
+      ok: false,
+      err: "Es necesario el Token de authenticacion",
+    });
+  }
+
+  jwt.verify(token, "secreta", (err, decode) => {
+    if (err) {
+      return res.status(401).json({
+        ok: false,
+        err: {
+          message: "Token no valido",
+        },
+      });
+    }
+
+    req.user = decode.user;
+    next();
+  });
+};
