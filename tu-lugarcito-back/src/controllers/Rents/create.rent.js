56665c3844e04a56e568e256d4f6d392cb45132b
nobody <moisesalex132@gmail.com>
2021-09-28 17:41:43 -0600

    ♻REFACTOR: mejora de controladores para optimizar

--- tu-lugarcito-back/src/controllers/Rents/create.rent.js
+++ tu-lugarcito-back/src/controllers/Rents/create.rent.js
@@ -14,14 +14,18 @@ exports.Create_rent = async (req, res) => {
     /** image fields */
     const cover_page = req.files['cover_page'][0].path;
     const gallery = req.files['gallery'];
-   
+
     let id_user = req.params.id;
     let body = req.body;
-    
+
     /** variable where we store the data that cloudinary sends us*/
     let result_gallery;
 
     try {
+
+        let type_home = (await Home_Type.findOne({ where: { type_of_rental: body.type_of_rental } }) || await Home_Type.create({ type_of_rental: body.type_of_rental }))
+        let type_business = (await Business_Type.findOne({ where: { type_offer: body.offer } }) || await Business_Type.create({ type_offer: body.offer }));
+
         const estate = await Estate.create({
             price: body.price,
             bedrooms: body.bedrooms,
@@ -30,8 +34,10 @@ exports.Create_rent = async (req, res) => {
             garage: body.garage,
             address: body.address,
             description: body.description,
-            id_user: id_user
-        })
+            id_user: id_user,
+            id_home: type_home.id,
+            id_business: type_business.id
+        });
 
         const result_cover_page = await cloudinary.v2.uploader.upload(cover_page);
         await Photos.create({ cover_page: result_cover_page.url, estate_id: estate.id });
@@ -41,29 +47,19 @@ exports.Create_rent = async (req, res) => {
             await Photos.create({ images: result_gallery.url, estate_id: estate.id });
             fs.unlinkSync(gallery[i].path);
         }
-
-        const type_home = await Home_Type.create({ type_of_rental: body.type_of_rental, estate_id: estate.id });
-        const type_business = await Business_Type.create({ offer: body.offer, estate_id: estate.id });
-
-
-        const all_state_images = await Photos.findAll({ where: { estate_id: estate.id } });
-
         fs.unlinkSync(req.files['cover_page'][0].path);
 
         res.status(201).json({
             ok: true,
             message: "Sitio publicado exelentemente",
             estate,
-            type_home,
-            type_business,
-            all_state_images
         })
 
 
     } catch (error) {
         res.status(500).json({
             ok: false,
-           error
+            error
         })
     }
 
