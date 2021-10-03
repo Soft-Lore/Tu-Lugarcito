7ebdb1818c0591c1a95bd5fbdac0ce96745c98d5
DixonDB <drcblandon@gmail.com>
2021-10-02 19:28:41 -0600

    Enlaces a redes sociales

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -4,18 +4,18 @@ export default function Footer() {
     return (
                 <footer className="footer">
                 <p className="footer-paragraph">Todos los derechos reservados</p>   
-                <h3 className="footer-title">TEAM FORANEOS</h3>
+                <h3 className="footer-title">TEAM_FORANEOS °_°</h3>
                 <div className="footer-line"></div>
                 <div className="footer-items">
-                    <a href="https://google.com" className="footer-ancla footer-ancla__fb">
+                    <a href="https://facebook.com/Tu_Lugarcito-103740648742221/?ref=pages_you_manage" className="footer-ancla footer-ancla__fb">
                         <img className="footer-ancla__icon" src="https://cdn.icon-icons.com/icons2/2429/PNG/512/facebook_logo_icon_147291.png" alt="facebook" />
                         <span className="footer-ancla__text footer-ancla__fb-text">Facebook</span>
                     </a>
-                    <a href="https://google.com" className="footer-ancla footer-ancla__ig">
+                    <a href="https://instagram.com/tu_lugarcito07/?hl=es" className="footer-ancla footer-ancla__ig">
                         <img className="footer-ancla__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1025px-Instagram-Icon.png" alt="instagram" />
                         <span className="footer-ancla__text footer-ancla__ig-text">Instagram</span>
                     </a>
-                    <a href="https://google.com" className="footer-ancla footer-ancla__tk">
+                    <a href="https://vm.tiktok.com/ZMRGuqA3t/" className="footer-ancla footer-ancla__tk">
                         <img className="footer-ancla__icon" src="https://cdn.worldvectorlogo.com/logos/tiktok-share-icon-black-1.svg" alt="tiktok" />
                         <span className="footer-ancla__text footer-ancla__ig-text">Tik Tok</span>
                     </a>
